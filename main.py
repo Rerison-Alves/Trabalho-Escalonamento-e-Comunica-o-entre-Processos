56d74d4c745ea76627414999104b52ee6d6905bd
João Victor de S. Albuquerque <84153487+J0hnV1ct0r@users.noreply.github.com>
2023-04-10 16:13:36 -0300

    implementando os graficos

--- main.py
+++ main.py
@@ -53,17 +53,48 @@ class EscalonadorRoundRobin:
 
 # Teste da simulação
 if __name__ == '__main__':
-    processos = [Processo(1, 10), Processo(2, 5), Processo(3, 8), Processo(4, 7)]
-    quantum = 2
+     quantums = range(1,31)
+    # Listas para armazenar os resultados de cada simulação
+    tempos_medios_espera = []
+    tempos_medios_retorno = []
+    vazoes = []
+    #teste de simulações
+    for quantum in quantums:
+      processos = [Processo(1, 10), Processo(2, 5), Processo(3, 8), Processo(4, 7)]
 
-    for p in processos:
+      for p in processos:
         p.burst += 1  # adiciona 1 unidade de tempo para mudança de contexto
+      escalonador = EscalonadorRoundRobin(quantum)
 
-    escalonador = EscalonadorRoundRobin(quantum)
-    for p in processos:
+      for p in processos:
         escalonador.adicionar_processo(p)
-    escalonador.executar_todos()
+      escalonador.executar_todos()
 
-    print(f'Tempo médio de espera: {escalonador.tempo_medio_espera()}')
-    print(f'Tempo médio de retorno: {escalonador.tempo_medio_retorno()}')
-    print(f'Vazão: {escalonador.vazao()} processos por unidade de tempo')
+      tempos_medios_espera.append(escalonador.tempo_medio_espera())
+      tempos_medios_retorno.append(escalonador.tempo_medio_retorno())
+      vazoes.append(escalonador.vazao())
+
+      print(f'Tempo médio de espera no quantum {quantum}: {escalonador.tempo_medio_espera()}')
+      print(f'Tempo médio de retorno no quantum {quantum}: {escalonador.tempo_medio_retorno()}')
+      print(f'Vazão no quantum {quantum}: {escalonador.vazao()} processos por unidade de tempo')
+
+    # Plotar gráfico de tempo médio de espera
+    sns.lineplot(x=quantums, y=tempos_medios_espera)
+    plt.title('Tempo médio de espera para diferentes valores de quantum')
+    plt.xlabel('Quantum')
+    plt.ylabel('Tempo médio de espera')
+    plt.show()
+
+    # Plotar gráfico de tempo médio de retorno
+    sns.lineplot(x=quantums, y=tempos_medios_retorno)
+    plt.title('Tempo médio de retorno para diferentes valores de quantum')
+    plt.xlabel('Quantum')
+    plt.ylabel('Tempo médio de retorno')
+    plt.show()
+
+    # Plotar gráfico de vazão
+    sns.lineplot(x=quantums, y=vazoes)
+    plt.title('Vazão para diferentes valores de quantum')
+    plt.xlabel('Quantum')
+    plt.ylabel('Vazão')
+    plt.show()
